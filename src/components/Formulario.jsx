9ccb96098a2486d2c188eb304c455c9b1fd47c3d
ing-mgiordano <ing.mgiordano@gmail.com>
2022-06-19 12:25:13 +0200

    se crea formato css, se crea fomulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,11 +1,52 @@
+import { useState } from "react"
 
 
 const Formulario = () => {
-  return (
-    <div>
-      form
-    </div>
-  )
+
+    const [busqueda, setBusqueda] = useState({
+        artista: '',
+        cancion: ''
+    })
+
+    return (
+        <form>
+            <legend>
+                Busca por Artista y Canción
+            </legend>
+
+            <div className="form-grid">
+                <div>
+                    <label>Artista</label>
+                    <input
+                        type="text"
+                        name="artista"
+                        placeholder="Nombre Artista"
+                        value={busqueda.artista}
+                        onChange={e => setBusqueda({
+                            ...busqueda,
+                            [e.target.name]: [e.target.value]
+                        })}
+                    />
+                </div>
+
+                <div>
+                    <label>Canción</label>
+                    <input
+                        type="text"
+                        name="cancion"
+                        placeholder="Nombre Canción"
+                        value={busqueda.cancion}
+                        onChange={e => setBusqueda({
+                            ...busqueda,
+                            [e.target.name]: [e.target.value]
+                        })}
+                    />
+                </div>
+
+                <input type="submit" value="Buscar" />
+            </div>
+        </form>
+    )
 }
 
 export default Formulario
