720aaf10bd3f6ead44d851800b6ea5a31dd2671c
ing-mgiordano <ing.mgiordano@gmail.com>
2022-06-19 12:04:58 +0200

    new py

---
+++ src/components/AppLetras.jsx
@@ -0,0 +1,17 @@
+import Formulario from "./Formulario"
+
+const AppLetras = () => {
+  return (
+    <>
+      <header>Búscador de Letras de Canciones</header>
+
+      <Formulario />
+
+      <main>
+
+      </main>
+    </>
+  )
+}
+
+export default AppLetras
